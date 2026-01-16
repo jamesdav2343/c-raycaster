bbacb6cda88dde2c713cba0b1b2d55ca8d67f43a
james <jamesmichaeldavies@outlook.com>
2026-01-16 23:36:17 +0000

    Working build with cmake

--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
+#include <SDL3/SDL.h>
 #include <stdio.h>
 
-int main() {
-    printf("hello world");
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char *args[])
+{
+    SDL_Window *window = NULL;
+    SDL_Surface *screenSurface = NULL;
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+        return 1;
+    }
+    window = SDL_CreateWindow(
+        "An SDL3 window", // window title
+        640,              // width, in pixels
+        480,              // height, in pixels
+        SDL_WINDOW_OPENGL // flags - see below
+    );
+
+    if (window == NULL)
+    {
+        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    printf("raycasting engine code running");
+
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
-
