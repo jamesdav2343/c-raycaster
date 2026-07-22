c26970a709e174b84eb418a9ac107be70962cdfe
Coderman <116694196+jamesdav2343@users.noreply.github.com>
2026-07-22 21:54:48 +0100

    fixed wall texture drawing

--- src/systems/raycaster.c
+++ src/systems/raycaster.c
@@ -116,11 +116,12 @@ static void write_vertical_wall_strip(Ray* ray, const Position* position, const 
 
     double tex_step = 1.0 * tex_height / line_height;
 
-    double tex_pos
-        = (draw_start - pitch->value - (pos_z / ray->perp_wall_dist) - buffer_height / 2 + line_height / 2) * tex_step;
+    double tex_pos = (draw_start - pitch->value - buffer_height / 2 + line_height / 2) * tex_step;
 
     float base_lighting_level = 1.0f
         - get_wall_light_intensity(ray->wall.position.x, ray->wall.position.y, ray->direction, ray->wall.orientation);
+
+    int pos = ray->wall.position.x + (ray->wall.position.y * COLS);
 
     for (int y = draw_start; y < draw_end; y++) {
         // Masked with tex_height - 1 in case of overflow
